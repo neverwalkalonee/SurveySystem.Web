6ef4acb75c33dd41b408ce64a18b3322ddf5d3af
Cihan <Cihan@DESKTOP-568GQ87>
2024-08-15 11:32:28 +0300

    Add project files.

---
+++ SurveySystem.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SurveySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveySystem.Repositories
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }            
+        public DbSet<SurveyField> SurveyFields { get; set; }
+    }
+    
+}
---
+++ SurveySystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurveySystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
